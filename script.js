57cb47987776ea000c73642914ae3b33bf4776b2
NatalyBMota <138526575+NatalyBMota@users.noreply.github.com>
2023-11-17 21:54:00 -0600

    Added pop-up alerts that tell the visitor when a radio button has been clicked, as well as when a submit link has been pressed.

--- script.js
+++ script.js
@@ -11,7 +11,7 @@ window.addEventListener('DOMContentLoaded', event => {
 
     // Navbar shrink function
     var navbarShrink = function () {
-        const navbarCollapsible = document.body.querySelector('#mainNav');
+        const navbarCollapsible = document.querySelector('#mainNav');
         if (!navbarCollapsible) {
             return;
         }
@@ -30,7 +30,7 @@ window.addEventListener('DOMContentLoaded', event => {
     document.addEventListener('scroll', navbarShrink);
 
     //  Activate Bootstrap scrollspy on the main nav element
-    const mainNav = document.body.querySelector('#mainNav');
+    const mainNav = document.querySelector('#mainNav');
     if (mainNav) {
         new bootstrap.ScrollSpy(document.body, {
             target: '#mainNav',
@@ -39,7 +39,7 @@ window.addEventListener('DOMContentLoaded', event => {
     };
 
     // Collapse responsive navbar when toggler is visible
-    const navbarToggler = document.body.querySelector('.navbar-toggler');
+    const navbarToggler = document.querySelector('.navbar-toggler');
     const responsiveNavItems = [].slice.call(
         document.querySelectorAll('#navbarResponsive .nav-link')
     );
@@ -51,4 +51,64 @@ window.addEventListener('DOMContentLoaded', event => {
         });
     });
 
+    /* Form processing for modal 1 */
+    var wantsToOrder = "";
+    var yesToOrderingProd1 = document.getElementById("y_to_order1");
+    var noToOrderingProd1 = document.getElementById("n_to_order1");
+
+    function doWhenYesRadioBtnIsClicked () {
+        alert("Yes");
+        wantsToOrder = true;
+    } 
+
+    function doWhenNoRadioBtnIsClicked() {
+        alert("No");
+        wantsToOrder = false;
+    }
+
+    yesToOrderingProd1.addEventListener('click', doWhenYesRadioBtnIsClicked);
+    noToOrderingProd1.addEventListener('click', doWhenNoRadioBtnIsClicked);
+
+    var submit1 = document.getElementById('submit-anchor');
+    alert(submit1);
+    function onSubmitBtn () {
+        //e.preventDefault();
+        
+        alert('Button clicked!');
+        
+        /*
+        for (var i = 0; i < radioInput.length; i++) {
+            var isChecked = radioInput[i].;
+            console.log(isChecked);
+        }
+        */
+    }
+    submit1.addEventListener('click', onSubmitBtn);
+
+
+
+    /* Form processing for modal 2 */
+    wantsToOrder = "";
+    var yesToOrderingProd2 = document.getElementById("y_to_order2");
+    var noToOrderingProd2 = document.getElementById("n_to_order2");
+
+    yesToOrderingProd2.addEventListener('click', doWhenYesRadioBtnIsClicked);
+    noToOrderingProd2.addEventListener('click', doWhenNoRadioBtnIsClicked);
+
+    var submit2 = document.getElementById('submit-anchor2');
+    submit2.addEventListener('click', onSubmitBtn);
+
+
+
+    /* Form processing for modal 3 */
+    wantsToOrder = "";
+    var yesToOrderingProd2 = document.getElementById("y_to_order3");
+    var noToOrderingProd2 = document.getElementById("n_to_order3");
+
+    yesToOrderingProd2.addEventListener('click', doWhenYesRadioBtnIsClicked);
+    noToOrderingProd2.addEventListener('click', doWhenNoRadioBtnIsClicked);
+
+    var submit3 = document.getElementById('submit-anchor3');
+    submit3.addEventListener('click', onSubmitBtn);
 });
+
